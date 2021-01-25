d45690ff337e238cce844e66050a8f3b7312aabd
jods4 <joel.dumas@elca.ch>
2021-01-25 01:47:16 +0100

    Quick bail-out when auto is not imported from vue

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import { init, parse as parseImports } from "es-module-lexer/dist/lexer.js";
 import { parse, print } from "recast";
 import transform from "./AutoTransform.js";
 import * as parser from "./parser.js";
+
+await init;
 
 export default function AutoRefPlugin() {
   return {
@@ -11,6 +14,16 @@ export default function AutoRefPlugin() {
       if (/\bnode_modules\b/i.test(id)) return;
       // Only process source code
       if (!/\.(ts|js|vue)$/i.test(id)) return;
+
+      // Detect if there is an `import { auto } from 'vue'` at all in the source
+      // and quickly bails out if there is none.
+      // es-module-lexer is much faster at this task than doing a full AST parse.
+      const [imports] = parseImports(src);
+      if (!imports.some(x => x.d === -1 // static import
+                          && x.e - x.s === 3
+                          && src.substring(x.s, x.e) === "vue" 
+                          && /\bauto\b/.test(src.substring(x.ss, x.se))))
+        return;
 
       return print(transform(parse(src, { parser })));
     }
